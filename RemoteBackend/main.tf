ed528d66795065d6c1d7a83ee2a5b68b58db5308
anudeepreddys <40909960+anudeepreddys@users.noreply.github.com>
2023-03-23 03:38:36 -0500

    Update main.tf

--- RemoteBackend/main.tf
+++ RemoteBackend/main.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name = var.rsgname
-    storage_account_name = var.stgactname
+    resource_group_name = "tfstateRG01"
+    storage_account_name = "tfstate01647403179"
     container_name = "tfstate"
     key = "terraform.tfstate"
   }
